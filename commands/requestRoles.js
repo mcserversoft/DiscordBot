9b828d67a43bf351ad5635a9c6be6114b308688f
LostAndDead <jakemarsh9012@gmail.com>
2020-04-08 16:25:33 +0100

    added all option for roles

--- commands/requestRoles.js
+++ commands/requestRoles.js
@@ -33,6 +33,24 @@ module.exports.run = async(bot, message, args) => {
                 .setColor("0x7ba73f");
                 message.channel.send(embed); 
             }
+        }else if(args[0].toLowerCase() == "all"){
+            var DevRole = message.guild.roles.cache.find(role => role.id == process.env.DEVROLEID);
+            var ReleaseRole = message.guild.roles.cache.find(role => role.id == process.env.RELEASESROLEID);
+            if(message.member.roles.cache.has(process.env.DEVROLEID) || message.member.roles.cache.has(process.env.RELEASESROLEID)){
+                message.member.roles.remove(DevRole,"BOT - Request Command").catch(console.error);
+                message.member.roles.remove(ReleaseRole,"BOT - Request Command").catch(console.error);
+                let embed = new Discord.MessageEmbed()
+                .addField("All Release Role Removed","You will no longer be mentioned when there is a new release or dev release.")
+                .setColor("0x7ba73f");
+                message.channel.send(embed); 
+            }else{
+                message.member.roles.add(DevRole,"BOT - Request Command");
+                message.member.roles.add(ReleaseRole,"BOT - Request Command");
+                let embed = new Discord.MessageEmbed()
+                .addField("All Release Role Added","You will now be mentioned when there is a new dev release or any kind.")
+                .setColor("0x7ba73f");
+                message.channel.send(embed);
+            }
         }
     }
 };
