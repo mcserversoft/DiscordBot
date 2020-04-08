4362f9c3e9993b34ae2c559ea8524b9fa0c6a325
LostAndDead <jakemarsh9012@gmail.com>
2020-04-08 16:30:12 +0100

    changed command args and trigger (again)

--- commands/requestRoles.js
+++ commands/requestRoles.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 //require('dotenv').config()
 module.exports.run = async(bot, message, args) => {
     if(message.guild.id == process.env.GUILDID){
-        if(args[0].toLowerCase() == "release"){
+        if(args[0].toLowerCase() == "release-news"){
             var ReleaseRole = message.guild.roles.cache.find(role => role.id == process.env.RELEASESROLEID);
             if(message.member.roles.cache.has(process.env.RELEASESROLEID)){
                 message.member.roles.remove(ReleaseRole,"BOT - Request Command").catch(console.error);
@@ -18,7 +18,7 @@ module.exports.run = async(bot, message, args) => {
                 .setColor("0x7ba73f");
                 message.channel.send(embed); 
             }
-        }else if(args[0].toLowerCase() == "dev"){
+        }else if(args[0].toLowerCase() == "dev-news"){
             var DevRole = message.guild.roles.cache.find(role => role.id == process.env.DEVROLEID);
             if(message.member.roles.cache.has(process.env.DEVROLEID)){
                 message.member.roles.remove(DevRole,"BOT - Request Command").catch(console.error);
@@ -33,21 +33,21 @@ module.exports.run = async(bot, message, args) => {
                 .setColor("0x7ba73f");
                 message.channel.send(embed); 
             }
-        }else if(args[0].toLowerCase() == "all"){
+        }else if(args[0].toLowerCase() == "all-news"){
             var DevRole = message.guild.roles.cache.find(role => role.id == process.env.DEVROLEID);
             var ReleaseRole = message.guild.roles.cache.find(role => role.id == process.env.RELEASESROLEID);
             if(message.member.roles.cache.has(process.env.DEVROLEID) || message.member.roles.cache.has(process.env.RELEASESROLEID)){
                 message.member.roles.remove(DevRole,"BOT - Request Command").catch(console.error);
                 message.member.roles.remove(ReleaseRole,"BOT - Request Command").catch(console.error);
                 let embed = new Discord.MessageEmbed()
-                .addField("All Release Role Removed","You will no longer be mentioned when there is a new release or dev release.")
+                .addField("All Release Role Removed","You will no longer be mentioned when there is a new release of any kind.")
                 .setColor("0x7ba73f");
                 message.channel.send(embed); 
             }else{
                 message.member.roles.add(DevRole,"BOT - Request Command");
                 message.member.roles.add(ReleaseRole,"BOT - Request Command");
                 let embed = new Discord.MessageEmbed()
-                .addField("All Release Role Added","You will now be mentioned when there is a new dev release or any kind.")
+                .addField("All Release Role Added","You will now be mentioned when there is a new release or any kind.")
                 .setColor("0x7ba73f");
                 message.channel.send(embed);
             }
@@ -56,5 +56,5 @@ module.exports.run = async(bot, message, args) => {
 };
 
 module.exports.help = {
-    name: "release-news"
+    name: "request"
 };
