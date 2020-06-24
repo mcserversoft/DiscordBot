556062c4a8e75e19cbcd804c16b20acd99fef48a
BuildTools <unconfigured@null.spigotmc.org>
2020-06-24 12:35:42 +0100

    Changed Order Of Checks In Purge

--- commands/purge.js
+++ commands/purge.js
@@ -4,36 +4,27 @@ const Discord = require("discord.js");
 //require('dotenv').config()
 module.exports.run = async(bot, message, args) => {
     //check if the arg provided is a number
-    if(!args[0] || args[0] <= 0 || args[0] >= 100){return message.channel.send("Please Specify A Number Between 1 - 99 (This is due to the discord API (Messages older than 14 days also can not be removed))")}
-    if(isNaN(args[0])){
-        message.channel.bulkDelete(ammount);
-        let embed = new Discord.MessageEmbed()
-        .addField("Error",`NaN: Not A Number`)
-        .setColor("0x7ba73f");
-        return message.channel.send(embed)
-    }
+    if(!message.member.roles.cache.has(process.env.STAFFROLEID)){return}
+    if(message.channel.type == "dm"){return}
+    if(!args[0] || args[0] <= 0 || args[0] >= 100){return message.channel.send("Please Specify A Number Between 1 - 99 (This is due to the discord API, Messages older than 14 days also can not be removed)")}
     let ammount;
-    //check if they have the staff role
-    if(message.member.roles.cache.has(process.env.STAFFROLEID) && message.channel.type != "dm"){
-        ammount = parseInt(args[0]) + 1
-        message.channel.fetch({
-            limit: ammount.toString()
-        })
-        .then(messages => {
-            try{
-                message.channel.bulkDelete(ammount);
-                let embed = new Discord.MessageEmbed()
-                .addField("Messages Purged",`Deleted ${ammount-1} messages\nThis message will self destruct in 5 seconds`)
-                .setColor("0x7ba73f");
-                message.channel.send(embed).then(msg => msg.delete({timeout: 5000}))
-            }
-            catch{
-                message.channel.send("Error: Missing Perms")
-            }
-        });
-    }else{
-        return;
-    }
+    //fetch discord channel history
+    ammount = parseInt(args[0]) + 1
+    message.channel.fetch({
+        limit: ammount.toString()
+    })
+    .then(messages => {
+        try{
+            message.channel.bulkDelete(ammount);
+            let embed = new Discord.MessageEmbed()
+            .addField("Messages Purged",`Deleted ${ammount-1} messages\nThis message will self destruct in 5 seconds`)
+            .setColor("0x7ba73f");
+            message.channel.send(embed).then(msg => msg.delete({timeout: 5000}))
+        }
+        catch{
+            message.channel.send("Error: Missing Perms")
+        }
+    });
 };
 
 module.exports.help = {
